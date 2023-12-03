d3941b61d49a5f3603f586dfcd1704919d27de93
Mr-H-Mehdi <110087564+Mr-H-Mehdi@users.noreply.github.com>
2023-12-03 14:46:31 +0500

    Add method to get details of an address.
    The program now takes a seed url, then linkFollower function starts crawling. It gets details about each of the anchor tag on the seed URL through getDetails function and then displays it.

--- scripts/index.php
+++ scripts/index.php
@@ -1,9 +1,71 @@
 <?php
 $seedURL="http://localhost/Assignments/WebSpider-01/html/testLinks.html";
 
-function linkFollower($url){
+$crawled=array();
+$crawling=array(); 
 
-    $content=file_get_contents($url);
+
+/**
+ * this function takes a link to return its corresponding details
+ * 
+ *Requires: @param string $url gives link of website to the function
+ *Effect:
+ * @return string containing title, decription, keywords and  url in json format(may change later on)
+ */
+function getDetails($url){
+    $options=array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: HMKsBot/0.01\n")) ;
+    //for managing the headers of the files
+    $context=stream_context_create($options);
+    
+    $content=@file_get_contents($url, false, $context);
+    if ($content==false){
+        // echo "Error: Link's Content Could Not Be Fetched.";
+        return;
+    }
+    $doc=new DOMDocument();
+    @$doc->loadHTML($content);
+
+    //extracting the page's title
+    $title = $doc->getElementsByTagName("title")->item(0)->nodeValue;
+    if ($title==="") {
+        # code...
+        echo "no title <br>";
+        return "nothing";
+    }
+    //extracting description and keywords from meta tags
+    $description= "";
+    $keywords= "";
+    $metas= $doc->getElementsByTagName("meta");
+    for( $i= 0; $i < $metas->length; $i++ ){
+        $meta = $metas->item($i);
+        if($meta->getAttribute("name") == strtolower("description")){
+            $description=$meta->getAttribute("content");
+        }
+        if($meta->getAttribute("name") == strtolower("keywords")){
+            $keywords=$meta->getAttribute("content");
+        }
+    }
+
+    return '{"Title": "'.$title.'", "Description": "'.str_replace("\n", "", $description).'", "Keywords" : "'.$keywords.'", "URL": "'.$url.'"}';
+}
+
+
+/**
+ * this function takes a link as seed to display its child link details with recursive calls
+ * 
+ * Requires: @param string $url gives link of seed URL to the function
+ * Effect: displays the results of crawling in the form of json array
+ * @return nothing
+ */
+function linkFollower($url){
+    global $crawled;
+    global $crawling;
+
+    $options=array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: HMKsBot/0.01\n")) ;
+    //for managing the headers of the files
+    $context=stream_context_create($options);
+
+    $content=@file_get_contents($url, false, $context);
     if ($content==false){
         echo "Error: Links Could Not Be Fetched.";
         return;
@@ -14,7 +76,7 @@ function linkFollower($url){
     $linkQueue = $doc->getElementsByTagName("a");
     foreach ($linkQueue as $link) {
         # code...
-        $l = $link->getAttribute("href");
+        $l = @$link->getAttribute("href");
 
         /** conditionals to handle different formats of links **/
         
@@ -47,8 +109,19 @@ function linkFollower($url){
             $l=parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
         }
         
-        
-        echo $l."<br>";
+        if (!in_array($l, $crawled)) {
+            $crawled[]=$l;
+            $crawling[]=$l;
+            echo $l."<br>";
+            // print_r($l."<br>");
+            // echo "<br><br><br>";
+            echo getDetails($l)."<br>";
+        }
+        //recursive crawling, keeps going to the child link
+        array_shift($crawling);
+        foreach($crawling as $site) {
+            linkFollower($site);
+        }
     }
 }
 linkFollower($seedURL);
